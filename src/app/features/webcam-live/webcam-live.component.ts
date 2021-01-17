e32e4fccb5b136e131ccfc11ad81fd1e6813f038
Ramu Yedla <ramu.yedla@planonsoftware.com>
2021-01-17 21:24:37 +0530

    prev changes

--- src/app/features/webcam-live/webcam-live.component.ts
+++ src/app/features/webcam-live/webcam-live.component.ts
@@ -4,29 +4,29 @@ declare const MediaRecorder: any;
 @Component({
   selector: 'app-webcam-live',
   templateUrl: './webcam-live.component.html',
-  styleUrls: ['./webcam-live.component.scss']
+  styleUrls: ['./webcam-live.component.scss'],
 })
 export class WebCamLiveComponent implements OnInit {
   @ViewChild('video', { static: true }) videoElement: ElementRef;
-  @ViewChild("canvas", { static: true }) public canvas: ElementRef;
+  @ViewChild('canvas', { static: true }) public canvas: ElementRef;
   public captures: any[] = [];
   constraints = {
     video: true,
-    audio: true
+    audio: true,
   };
   private recordedBlobs = [];
   private currentStream;
   private mediaRecorder;
   public recordingStarted = false;
   private liveDetectionVideoElem: HTMLVideoElement;
-  constructor(private renderer: Renderer2) { }
+  constructor(private renderer: Renderer2) {}
 
   ngOnInit() {
     this.initCamera();
   }
   tabChanged(event) {
     if (event.index === 0) {
-     this.initCamera();
+      this.initCamera();
     } else {
       this.webcam_init();
       this.predictWithCocoModel();
@@ -34,7 +34,10 @@ export class WebCamLiveComponent implements OnInit {
   }
   private initCamera() {
     if (!!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)) {
-      navigator.mediaDevices.getUserMedia(this.constraints).then(stream => this.attachVideo(stream)).catch(this.handleError);
+      navigator.mediaDevices
+        .getUserMedia(this.constraints)
+        .then((stream) => this.attachVideo(stream))
+        .catch(this.handleError);
     } else {
       alert('Sorry, camera not available.');
     }
@@ -87,21 +90,21 @@ export class WebCamLiveComponent implements OnInit {
   }
 
   public capturePhoto() {
-    this.canvas.nativeElement.getContext("2d").drawImage(this.videoElement.nativeElement, 0, 0, 640, 480);
-    this.captures.push(this.canvas.nativeElement.toDataURL("image/png"));
+    this.canvas.nativeElement.getContext('2d').drawImage(this.videoElement.nativeElement, 0, 0, 640, 480);
+    this.captures.push(this.canvas.nativeElement.toDataURL('image/png'));
   }
 
   webcam_init() {
-    this.liveDetectionVideoElem = <HTMLVideoElement>document.getElementById("liveDetectionVideo");
+    this.liveDetectionVideoElem = <HTMLVideoElement>document.getElementById('liveDetectionVideo');
 
     navigator.mediaDevices
       .getUserMedia({
         audio: false,
         video: {
-          facingMode: "user",
-        }
+          facingMode: 'user',
+        },
       })
-      .then(stream => {
+      .then((stream) => {
         this.liveDetectionVideoElem.srcObject = stream;
         this.liveDetectionVideoElem.onloadedmetadata = () => {
           this.liveDetectionVideoElem.play();
@@ -109,56 +112,59 @@ export class WebCamLiveComponent implements OnInit {
       });
   }
   public async predictWithCocoModel() {
-    const model = await cocoSSD.load({ base: 'lite_mobilenet_v2'});
+    const model = await cocoSSD.load({ base: 'lite_mobilenet_v2' });
     this.detectFrame(this.liveDetectionVideoElem, model);
     console.log('model loaded');
   }
   detectFrame = (video, model) => {
-    model.detect(video).then(predictions => {
-      this.renderPredictions(predictions);
-      requestAnimationFrame(() => {
-        this.detectFrame(video, model);
+    if (model && model.detect && video) {
+      model.detect(video).then((predictions) => {
+        this.renderPredictions(predictions);
+        requestAnimationFrame(() => {
+          this.detectFrame(video, model);
+        });
       });
-    });
-  }
+    }
+  };
 
-  renderPredictions = predictions => {
-    const canvas = <HTMLCanvasElement>document.getElementById("canvas-live-detection");
+  renderPredictions = (predictions) => {
+    const canvas = <HTMLCanvasElement>document.getElementById('canvas-live-detection');
+    if (canvas && canvas.getContext) {
+      const ctx = canvas.getContext('2d');
 
-    const ctx = canvas.getContext("2d");
+      canvas.width = 640;
+      canvas.height = 480;
 
-    canvas.width = 640;
-    canvas.height = 480;
+      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+      // Font options.
+      const font = '16px sans-serif';
+      ctx.font = font;
+      ctx.textBaseline = 'top';
+      ctx.drawImage(this.liveDetectionVideoElem, 0, 0, 640, 480);
 
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    // Font options.
-    const font = "16px sans-serif";
-    ctx.font = font;
-    ctx.textBaseline = "top";
-    ctx.drawImage(this.liveDetectionVideoElem, 0, 0, 640, 480);
+      predictions.forEach((prediction) => {
+        const x = prediction.bbox[0];
+        const y = prediction.bbox[1];
+        const width = prediction.bbox[2];
+        const height = prediction.bbox[3];
+        // Draw the bounding box.
+        ctx.strokeStyle = '#00FFFF';
+        ctx.lineWidth = 2;
+        ctx.strokeRect(x, y, width, height);
+        // Draw the label background.
+        ctx.fillStyle = '#00FFFF';
+        const textWidth = ctx.measureText(prediction.class).width;
+        const textHeight = parseInt(font, 10); // base 10
+        ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
+      });
 
-    predictions.forEach(prediction => {
-      const x = prediction.bbox[0];
-      const y = prediction.bbox[1];
-      const width = prediction.bbox[2];
-      const height = prediction.bbox[3];
-      // Draw the bounding box.
-      ctx.strokeStyle = "#00FFFF";
-      ctx.lineWidth = 2;
-      ctx.strokeRect(x, y, width, height);
-      // Draw the label background.
-      ctx.fillStyle = "#00FFFF";
-      const textWidth = ctx.measureText(prediction.class).width;
-      const textHeight = parseInt(font, 10); // base 10
-      ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
-    });
-
-    predictions.forEach(prediction => {
-      const x = prediction.bbox[0];
-      const y = prediction.bbox[1];
-      // Draw the text last to ensure it's on top.
-      ctx.fillStyle = "#000000";
-      ctx.fillText(prediction.class, x, y);
-    });
+      predictions.forEach((prediction) => {
+        const x = prediction.bbox[0];
+        const y = prediction.bbox[1];
+        // Draw the text last to ensure it's on top.
+        ctx.fillStyle = '#000000';
+        ctx.fillText(prediction.class, x, y);
+      });
+    }
   };
 }
